88b0fa00d87f3383aa81fba72557391448922562
badawi1713 <dzaky.badawi@gmail.com>
2021-05-14 16:10:59 +0700

    View's folders structure

---
+++ src/views/components/molecules/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='flex flex-row'>
+
+            <div>
+                <img />
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
