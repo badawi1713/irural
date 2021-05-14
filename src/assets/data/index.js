56dfe75f6348b4ff93d8a40c88b15a060e31cd84
badawi1713 <dzaky.badawi@gmail.com>
2021-05-14 15:26:30 +0700

    Add assets

---
+++ src/assets/data/index.js
@@ -0,0 +1,138 @@
+export const provinceList = [
+    {
+        id: 1,
+        name: "Aceh",
+    },
+    {
+        id: 2,
+        name: "Bali",
+    },
+    {
+        id: 3,
+        name: "Banten",
+    },
+    {
+        id: 4,
+        name: "Bengkulu",
+    },
+    {
+        id: 5,
+        name: "D.I Yogyakarta",
+    },
+    {
+        id: 6,
+        name: "Gorontalo",
+    },
+    {
+        id: 7,
+        name: "Jakarta",
+    },
+    {
+        id: 8,
+        name: "Jambi",
+    },
+    {
+        id: 9,
+        name: "Jawa Barat",
+    },
+    {
+        id: 10,
+        name: "Jawa Tengah",
+    },
+    {
+        id: 11,
+        name: "Jawa Timur",
+    },
+    {
+        id: 12,
+        name: "Kalimantan Barat",
+    },
+    {
+        id: 13,
+        name: "Kalimantan Selatan",
+    },
+    {
+        id: 14,
+        name: "Kalimantan Tengah",
+    },
+    {
+        id: 15,
+        name: "Kalimantan Timur",
+    },
+    {
+        id: 16,
+        name: "Kalimantan Utara",
+    },
+    {
+        id: 17,
+        name: "Kep. Bangka Belitung",
+    },
+    {
+        id: 18,
+        name: "Kepulauan Riau",
+    },
+    {
+        id: 19,
+        name: "Lampung",
+    },
+    {
+        id: 20,
+        name: "Maluku",
+    },
+    {
+        id: 21,
+        name: "Maluku Utara",
+    },
+    {
+        id: 22,
+        name: "Nusa Tenggara Barat",
+    },
+    {
+        id: 23,
+        name: "Nusa Tenggara Timur",
+    },
+    {
+        id: 24,
+        name: "Papua",
+    },
+    {
+        id: 25,
+        name: "Papua Barat",
+    },
+    {
+        id: 26,
+        name: "Riau",
+    },
+    {
+        id: 27,
+        name: "Sulawesi Barat",
+    },
+    {
+        id: 28,
+        name: "Sulawesi Selatan",
+    },
+    {
+        id: 29,
+        name: "Sulawesi Tengah",
+    },
+    {
+        id: 30,
+        name: "Sulawesi Tenggara",
+    },
+    {
+        id: 31,
+        name: " Sulawesi Utara",
+    },
+    {
+        id: 32,
+        name: "Sumatra Barat",
+    },
+    {
+        id: 33,
+        name: "Sumatra Selatan",
+    },
+    {
+        id: 34,
+        name: "Sumatra Utara",
+    },
+]
