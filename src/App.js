3e8a3fb883e01e304754f85892972c6baee380c0
badawi1713 <dzaky.badawi@gmail.com>
2021-05-14 16:20:47 +0700

    Navigation routers integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Home, Registration } from './views/pages'
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/register' component={Registration} />
+      </Switch>
+    </Router>
   )
 }
 
