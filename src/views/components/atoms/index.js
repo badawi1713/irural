ca8b889897ee79fd7423264afd6e31b11cb38f26
badawi1713 <dzaky.badawi@gmail.com>
2021-05-26 11:52:01 +0700

    Remove useless files

--- src/views/components/atoms/index.js
+++ src/views/components/atoms/index.js
@@ -1,6 +1,5 @@
-import Button from './Button'
 import Input from './Input'
 import Label from './Label'
-import Navlink from './Navlink'
 import Select from './Select'
-export { Button, Input, Label, Navlink, Select }
+export { Input, Label, Select }
+
